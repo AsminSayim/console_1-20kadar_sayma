4e64806d6c0dacbbdb1e95c43c249f6d913d8a80
AsminSayim <145451871+AsminSayim@users.noreply.github.com>
2024-02-05 10:38:42 +0300

    Add files via upload

---
+++ Console_ornek/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0; i < 20; i++)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadKey();
+        }
+    }
+}
